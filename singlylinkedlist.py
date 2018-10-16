944d7c556383799f67e28e1efd4ac62cc34eb08d
Joon-Ho Son <joon-ho.son17@imperial.ac.uk>
2018-10-16 23:37:46 +0100

    Started singlylinkedlist

---
+++ singlylinkedlist.py
@@ -0,0 +1,13 @@
+class Node:
+    
+    def __init__(self, data, next=None):
+        self.data = data
+        self.next = next
+
+# Could implement without this wrapper
+class SinglyLinkedList:
+    
+    
+
+    
+
